3b1e48d70fe1c7de9299f338fc585515d14f5038
mireiffe <mireiffe@kaist.ac.kr>
2021-11-12 23:36:06 +0900

    mdfy

--- _codes/weeks/week10/my_rref_blank.m
+++ _codes/weeks/week10/my_rref_blank.m
@@ -1,7 +1,6 @@
 function rref_A = my_rref(A)
 % Find the reduced row echelon form of A
 % by performing Gauss-Jordan elimination with partial pivoting.
-
 [m, n] = size(A);
 rref_A = A;             % Initialization of rref_A as A.
 
@@ -11,7 +10,7 @@ for colIdx = 1:n
     % Find element to interchange two rows.
     [maxEntry, maxIdx] = max(abs(rref_A(rowIdx:m, colIdx)));
     
-    if ---BLANK---      % If this column is a non-zero vector,
+    if abs(maxEntry) >= 10^-10    % If this column is a non-zero vector,
                         % then store index of pivot column.
                         % This part is more complicated than it looks.
                         % You may need to consider the 'round-off error'
@@ -32,8 +31,8 @@ for colIdx = 1:n
 end
 
 % Backward phase.
+preLding1Row = 0;
 for pc = ---BLANK---    % Find only the pivot columns.
-    preLding1Row = 0;
     
     % This is looking for the index of row in which leading 1 is apeared .
     for lding1Row = m - preLding1Row:-1:1
